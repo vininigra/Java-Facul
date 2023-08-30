6a038fd8e0e9d745b284a57a4c7682aa3758b8c6
Vinicius Augusto Nigra <111812339+vininigra@users.noreply.github.com>
2023-08-30 20:16:41 -0300

    atualizando

---
+++ java-aula-vini/src/domain/Lagarto.java
@@ -0,0 +1,28 @@
+package domain;
+
+public class Lagarto {
+    
+
+public class LAGARTO implements Algoritmo{
+
+    @Override
+    public void excutar(TipoAlgoritmo tipo) {
+        switch(tipo){
+            case LAGARTO-> System.out.println("Perdeu! Lagarto come o Papel");
+
+            case TESOURA-> System.out.println("Perdeu! Tesoura corta o Papel");
+
+            case PEDRA-> System.out.println("Ganhou! Papel embrulha Pedra");
+            
+            case SPOCK-> System.out.println("Ganhou! Papel refuta o Spock");
+
+            default -> System.out.println("Empatou! Papel empata com Papel");
+        }
+        
+    }
+
+    
+    
+}
+
+}
