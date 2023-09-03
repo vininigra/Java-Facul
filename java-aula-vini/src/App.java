fce69cb6cf00e592707853415cc838c5be6bf7b0
Vinicius Augusto Nigra <111812339+vininigra@users.noreply.github.com>
2023-09-03 12:27:36 -0300

    Atualizacao-Entrega-Prof

--- java-aula-vini/src/App.java
+++ java-aula-vini/src/App.java
@@ -5,16 +5,15 @@ import domain.Algoritmo;
 import domain.Jokenpo;
 import domain.Papel;
 import domain.Pedra;
+import domain.Spock;
+import domain.Lagarto;
 import domain.Tesoura;
 import domain.TipoAlgoritmo;
 
 public class App {
     
-
-   
-    
     public static void main(String[] args) throws Exception {
-        Scanner in = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Selecione uma das opções: ");
         System.out.println("PAPEL :1");
         System.out.println("TESOURA :2");
@@ -24,25 +23,25 @@ public class App {
         System.out.println();
 
         //TODO: Criar funcao para converter int em algoritmo
-        Algoritmo papel = new Papel();
 
+        Integer jogador = scanner.nextInt();
+        Algoritmo algoritmo = getAlgoritmo(jogador);
+    
         //TODO : Gerar jogada randomica e converter em um Enum
         Integer random = new Random().nextInt(5) + 1;
         TipoAlgoritmo computador = TipoAlgoritmo.getTipo(random);
         
         
-
         Jokenpo jokenpo = new Jokenpo();
-        jokenpo.setAlgoritmo(papel);
+        jokenpo.setAlgoritmo(algoritmo);
 
         jokenpo.Jogar(computador);
 
-        in.close();
+        scanner.close();
         
-       
     }
 
-    static Algoritmo geAlgoritmo(Integer id){
+    static Algoritmo getAlgoritmo(Integer id){
         TipoAlgoritmo tipo = TipoAlgoritmo.getTipo(id);
         switch(tipo){
             case PAPEL:
